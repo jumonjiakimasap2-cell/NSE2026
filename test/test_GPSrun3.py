4ac8276ed75de3240c7c2250c95e40e3d3ea2086
jumonjiakimasap2-cell <jumonji.akimasa.p2@dc.tohoku.ac.jp>
2026-08-09 19:56:21 +0900

    Update test_GPSrun3.py

--- test/test_GPSrun3.py
+++ test/test_GPSrun3.py
@@ -238,11 +238,11 @@ def calc_direction_diff(azimuth: float, target_bearing: float) -> float:
 # ===========================================================================
 
 PIN_PWMA = 13
-PIN_AIN1 =  5
-PIN_AIN2 =  6
+PIN_AIN1 =  6
+PIN_AIN2 =  5
 PIN_PWMB = 18
-PIN_BIN1 = 23
-PIN_BIN2 = 24
+PIN_BIN1 = 24
+PIN_BIN2 = 23
 PIN_STBY = 11  # STBYピン
 
 
